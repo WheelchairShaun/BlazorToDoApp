c1e86a51bb0461159c344420d0a598f4ceb4f2bf
Shaun Barnes <shaun.w.barnes@gmail.com>
2025-12-06 16:13:39 -0500

    Created Blazor project, todo item, and initial tests (#1)

---
+++ src/TodoApp.Tests/TodoItemTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using TodoApp;
+
+namespace TodoApp.Tests;
+
+public class TodoItemTests
+{
+    [Fact]
+    public void NewItem_IsNotCompleted()
+    {
+        TodoItem item = new("Test Task");
+        Assert.False(item.IsCompleted);
+    }
+
+    [Theory]
+    [InlineData("Task A")]
+    [InlineData("Task B")]
+    public void Title_IsRequired(string title)
+	{
+		TodoItem item = new(title);
+        Assert.Equal(title, item.Title);
+	}
+}
